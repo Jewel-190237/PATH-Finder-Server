e947f612410527db8a92b91720d12ba548cf1f2a
Azhar190238 <azhar73397@gmail.com>
2025-01-12 11:05:20 +0600

    Added

--- index.js
+++ index.js
@@ -782,9 +782,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('bus-ticket is running');
+    res.send('Pathfinder is running');
 })
 
 app.listen(PORT, () => {
-    console.log(`bus-ticket is running on ${PORT}`);
+    console.log(`Pathfinder is running on ${PORT}`);
 });
